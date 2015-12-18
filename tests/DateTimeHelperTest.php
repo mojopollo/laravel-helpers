22d722b7e4b214746d9806fb7d3e3ad5b0f4aea4
Jaime Medina <jmedina@qujo.com>
2015-12-18 13:59:47 -0800

    adds more tests to date time helper class

--- tests/DateTimeHelperTest.php
+++ tests/DateTimeHelperTest.php
@@ -123,4 +123,103 @@ class DateTimeHelperTest extends \PHPUnit_Framework_TestCase
     $this->assertEquals($result, $expectedResults);
   }
 
+  /**
+   * Test range() with monthly generation
+   *
+   * @return void
+   */
+  public function testRangeMonthly()
+  {
+    // Set parameters
+    $startDate = '2015-06-04 08:00:00';
+    $endDate = '2015-06-04 12:00:00';
+    $periodDate = '2015-09-04 08:00:00';
+    $step = '+1 month';
+    $daysOfWeek = null;
+    $daysOfWeekTimeZoneName = null;
+    $dateFormat = 'Y-m-d H:i:s';
+
+    // Execute method
+    $result = $this->dateTimeHelper->range($startDate, $endDate, $periodDate, $step, $daysOfWeek, $daysOfWeekTimeZoneName, $dateFormat);
+    fwrite(STDERR, print_r($result, true));
+
+    // Expected array
+    $expectedResults = [
+      [
+        'start' => '2015-06-04 08:00:00',
+        'end' => '2015-06-04 12:00:00',
+      ],
+      [
+        'start' => '2015-07-04 08:00:00',
+        'end' => '2015-07-04 12:00:00',
+      ],
+      [
+        'start' => '2015-08-04 08:00:00',
+        'end' => '2015-08-04 12:00:00',
+      ],
+      [
+        'start' => '2015-09-04 08:00:00',
+        'end' => '2015-09-04 12:00:00',
+      ],
+    ];
+
+    // Check: there should be a total of 12 total records created
+    $this->assertEquals($result, $expectedResults);
+  }
+
+  /**
+   * Test range() with weekly generation
+   *
+   * @return void
+   */
+  public function testRangeWeekly()
+  {
+    // Set parameters
+    $startDate = '2015-09-23 10:11:51';
+    $endDate = '2015-09-24 02:55:51';
+    $periodDate = '2015-09-30 11:59:59';
+    $step = '+1 week';
+    $daysOfWeek = 'mon,wed,fri';
+    $daysOfWeekTimeZoneName = null;
+    $dateFormat = 'Y-m-d H:i:s';
+
+    // Execute method
+    $result = $this->dateTimeHelper->range($startDate, $endDate, $periodDate, $step, $daysOfWeek, $daysOfWeekTimeZoneName, $dateFormat);
+    // fwrite(STDERR, print_r($result, true));
+
+    // Check: there should be a total of 6 total records created
+    $this->assertEquals(6, count($result));
+
+    // Expected array
+    $expectedResults = [
+      [
+        'start' => '2015-09-21 10:11:51',
+        'end' => '2015-09-22 02:55:51',
+      ],
+      [
+        'start' => '2015-09-23 10:11:51',
+        'end' => '2015-09-24 02:55:51',
+      ],
+      [
+        'start' => '2015-09-25 10:11:51',
+        'end' => '2015-09-26 02:55:51',
+      ],
+      [
+        'start' => '2015-09-28 10:11:51',
+        'end' => '2015-09-29 02:55:51',
+      ],
+      [
+        'start' => '2015-09-30 10:11:51',
+        'end' => '2015-10-01 02:55:51',
+      ],
+      [
+        'start' => '2015-10-02 10:11:51',
+        'end' => '2015-10-03 02:55:51',
+      ],
+    ];
+
+    // Check: there should be a total of 12 total records created
+    $this->assertEquals($result, $expectedResults);
+  }
+
 }
